d340df597f9ad239cc7a7495b981f90866b80a38
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2008-06-22 08:50:51 +0000

    removed redundant line in doctest

--- pycddlib.pyx
+++ pycddlib.pyx
@@ -35,7 +35,6 @@ This is the testlp2.c example that comes with cddlib.
 
 >>> import pycddlib
 >>> lp = pycddlib.Matrix([[4.0/3.0,-2,-1],[2.0/3.0,0,-1],[0,1,0],[0,0,1]])
->>> lp.setRepType(REP_INEQUALITY)
 >>> lp.setLPObjType(LPOBJ_MAX)
 >>> lp.setLPObjFunc([0,3,4])
 >>> print lp
