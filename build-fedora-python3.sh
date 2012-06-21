0ca48fd48c402fc308b0f71c6c299ba8b544179a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2012-06-21 10:18:58 +0100

    Copied build file.

---
+++ build-fedora-python3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+pushd .
+(
+git clean -xfd &&
+python2 setup.py build &&
+python2 setup.py install --user &&
+git clean -xfd &&
+python3 setup.py build &&
+python3 setup.py install --user &&
+pushd docs &&
+pushd _build/html &&
+(([ "`ls -1 | xargs`" ] && (ls -1 | xargs rm -r)) || true) &&
+popd &&
+make html &&
+make latexpdf &&
+make doctest &&
+popd  &&
+python3 setup.py sdist --formats=zip
+) || popd
+
