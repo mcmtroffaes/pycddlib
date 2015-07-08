4f718efb43ed0f3354c470e50b0fa01e20bf4e51
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2015-07-08 13:56:25 +0100

    Fix for Python 2.6 tests.

--- test/test_polyhedron.py
+++ test/test_polyhedron.py
@@ -20,7 +20,7 @@ def _test_sampleh1(number_type=None, assert_matrix_equal=None):
         list(ext),
         [(1, 0, 0, 0), (1, 2, 0, 0), (1, 0, 2, 0), (0, 0, 0, 1)])
     # note: first row is 0, so fourth row is 3
-    nose.tools.assert_list_equal(list(ext.lin_set), [3])
+    nose.tools.assert_equal(list(ext.lin_set), [3])
 
 def _test_testcdd2(number_type=None, assert_matrix_equal=None):
     mat = cdd.Matrix([[7,-3,-0],[7,0,-3],[1,1,0],[1,0,1]],
@@ -41,7 +41,7 @@ def _test_testcdd2(number_type=None, assert_matrix_equal=None):
     mat.extend([[7, -3, 1]])
     assert_matrix_equal(
         list(mat), [(7,-3,-0),(7,0,-3),(1,1,0),(1,0,1),(7,1,-3),(7,-3,1)])
-    nose.tools.assert_list_equal(list(mat.lin_set), [4])
+    nose.tools.assert_equal(list(mat.lin_set), [4])
     gen2 = cdd.Polyhedron(mat).get_generators()
     nose.tools.assert_equal(gen2.rep_type, cdd.RepType.GENERATOR)
     assert_matrix_equal(
@@ -49,13 +49,13 @@ def _test_testcdd2(number_type=None, assert_matrix_equal=None):
         [(1, -1, 2), (1, 0, Fraction(7, 3))])
 
 def test_fraction_sampleh1():
-    _test_sampleh1('fraction', nose.tools.assert_list_equal) 
+    _test_sampleh1('fraction', nose.tools.assert_equal)
 
 def test_float_sampleh1():
     _test_sampleh1('float', assert_almost_equal)
 
 def test_fraction_testcdd2():
-    _test_testcdd2('fraction', nose.tools.assert_list_equal) 
+    _test_testcdd2('fraction', nose.tools.assert_equal)
 
 def test_float_testcdd2():
     _test_testcdd2('float', assert_almost_equal)
