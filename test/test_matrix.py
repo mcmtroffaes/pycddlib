6e0a103518568693ae23bf8823d815d66363fe9a
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-01-15 19:48:37 +0000

    Start using pytest.

--- test/test_matrix.py
+++ test/test_matrix.py
@@ -1,14 +1,14 @@
 import cdd
-import nose
+import pytest
 from fractions import Fraction
 
 def assert_vector_almost_equal(vec1, vec2):
-    nose.tools.assert_equal(len(vec1), len(vec2))
+    assert len(vec1) == len(vec2)
     for (entry1, entry2) in zip(vec1, vec2):
-        nose.tools.assert_almost_equal(entry1, entry2)
+        assert entry1 == pytest.approx(entry2)
 
 def assert_matrix_almost_equal(mat1, mat2):
-    nose.tools.assert_equal(len(mat1), len(mat2))
+    assert len(mat1) == len(mat2)
     for (row1, row2) in zip(mat1, mat2):
         assert_vector_almost_equal(row1, row2)
 
@@ -28,12 +28,12 @@ def test_large_number_4():
     mat = cdd.Matrix([['10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000/17984638288961211871838956989189665890197130672912829203311075745019255958028927299020895173379216649']], number_type='float')
     assert_matrix_almost_equal(list(mat), [[0.556030087418433]])
 
-@nose.tools.raises(ValueError)
 def test_length():
-    mat = cdd.Matrix([[1], [1, 2]])
+    with pytest.raises(ValueError):
+        mat = cdd.Matrix([[1], [1, 2]])
 
-@nose.tools.raises(ValueError)
 def test_obj_func():
     mat = cdd.Matrix([[1], [2]])
-    mat.obj_func = (0, 0)
+    with pytest.raises(ValueError):
+        mat.obj_func = (0, 0)
 
--- test/test_polyhedron.py
+++ test/test_polyhedron.py
@@ -1,29 +1,40 @@
 import cdd
-import nose
+import pytest
 from fractions import Fraction
 from test_matrix import assert_matrix_almost_equal
 
-def _test_sampleh1(number_type=None, assert_matrix_equal=None):
+def assert_matrix_exactly_equal(a, b):
+    assert list(a) == list(b)
+
+@pytest.mark.parametrize(
+    "number_type,assert_matrix_equal",
+    [("fraction", assert_matrix_exactly_equal),
+     ("float", assert_matrix_almost_equal)])
+def test_sampleh1(number_type, assert_matrix_equal):
     mat = cdd.Matrix([[2,-1,-1,0],[0,1,0,0],[0,0,1,0]],
                      number_type=number_type)
     mat.rep_type = cdd.RepType.INEQUALITY
     poly = cdd.Polyhedron(mat)
     ext = poly.get_generators()
-    nose.tools.assert_equal(ext.rep_type, cdd.RepType.GENERATOR)
+    assert ext.rep_type == cdd.RepType.GENERATOR
     assert_matrix_equal(
         list(ext),
         [(1, 0, 0, 0), (1, 2, 0, 0), (1, 0, 2, 0), (0, 0, 0, 1)])
     # note: first row is 0, so fourth row is 3
-    nose.tools.assert_equal(list(ext.lin_set), [3])
+    assert list(ext.lin_set) == [3]
 
-def _test_testcdd2(number_type=None, assert_matrix_equal=None):
+@pytest.mark.parametrize(
+    "number_type,assert_matrix_equal",
+    [("fraction", assert_matrix_exactly_equal),
+     ("float", assert_matrix_almost_equal)])
+def test_testcdd2(number_type, assert_matrix_equal):
     mat = cdd.Matrix([[7,-3,-0],[7,0,-3],[1,1,0],[1,0,1]],
                      number_type=number_type)
     mat.rep_type = cdd.RepType.INEQUALITY
     assert_matrix_equal(
         list(mat), [(7,-3,-0),(7,0,-3),(1,1,0),(1,0,1)])
     gen = cdd.Polyhedron(mat).get_generators()
-    nose.tools.assert_equal(gen.rep_type, cdd.RepType.GENERATOR)
+    assert gen.rep_type == cdd.RepType.GENERATOR
     assert_matrix_equal(
         list(gen),
         [(1, Fraction(7, 3), -1),
@@ -35,21 +46,9 @@ def _test_testcdd2(number_type=None, assert_matrix_equal=None):
     mat.extend([[7, -3, 1]])
     assert_matrix_equal(
         list(mat), [(7,-3,-0),(7,0,-3),(1,1,0),(1,0,1),(7,1,-3),(7,-3,1)])
-    nose.tools.assert_equal(list(mat.lin_set), [4])
+    assert list(mat.lin_set) == [4]
     gen2 = cdd.Polyhedron(mat).get_generators()
-    nose.tools.assert_equal(gen2.rep_type, cdd.RepType.GENERATOR)
+    assert gen2.rep_type == cdd.RepType.GENERATOR
     assert_matrix_equal(
         list(gen2),
         [(1, -1, 2), (1, 0, Fraction(7, 3))])
-
-def test_fraction_sampleh1():
-    _test_sampleh1('fraction', nose.tools.assert_equal)
-
-def test_float_sampleh1():
-    _test_sampleh1('float', assert_matrix_almost_equal)
-
-def test_fraction_testcdd2():
-    _test_testcdd2('fraction', nose.tools.assert_equal)
-
-def test_float_testcdd2():
-    _test_testcdd2('float', assert_matrix_almost_equal)
