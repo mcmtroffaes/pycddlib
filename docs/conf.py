5ba3e99c8046e85576e1ac3063ad5ebf5777aa52
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-08 10:07:18 +0100

    Fix doc version.

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@ source_suffix = ".rst"
 master_doc = "index"
 project = "pycddlib"
 copyright = "2008-2024, Matthias C. M. Troffaes"
-release = importlib.metadata.version("bunnydroid")
+release = importlib.metadata.version("pycddlib")
 # short X.Y version
 version = ".".join(release.split(".")[:2])
 exclude_patterns = ["_build"]
