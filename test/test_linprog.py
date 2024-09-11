0a28358e9b20d2cb3a3b456d4dbecd6cb2d81e8f
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-11 12:09:42 +0100

    Assert initial solutions for lp before solve.

--- test/test_linprog.py
+++ test/test_linprog.py
@@ -21,6 +21,7 @@ def test_lin_prog_type() -> None:
         assert isinstance(xs, Sequence)
         for x in xs:
             assert isinstance(x, float)
+            assert x == 0.0
     assert lp.solve(solver=cdd.LPSolverType.CRISS_CROSS) is None
     assert_almost_equal(lp.obj_value, 1.5)
     assert_vector_almost_equal(lp.primal_solution, [0.5])
