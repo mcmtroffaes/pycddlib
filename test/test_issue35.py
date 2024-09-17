59ce64f0f688df04374e8ec80d6c934a7c3d51df
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-17 11:00:15 +0100

    Update test for v3.0.0.

--- test/test_issue35.py
+++ test/test_issue35.py
@@ -3,6 +3,5 @@ import cdd
 
 # this should not segfault
 def test_issue35():
-    m = cdd.Matrix([[0, 0, 0]], number_type="float")
-    m.rep_type = cdd.RepType.INEQUALITY
-    m.canonicalize()
+    mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
+    cdd.matrix_canonicalize(mat)
