f95a403f9e9cb619521280562821ec4eee88c7f3
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-17 16:35:36 +0100

    Reformat.

--- test/gmp/test_enums.py
+++ test/gmp/test_enums.py
@@ -6,9 +6,7 @@ import cdd
 import cdd.gmp
 
 
-@pytest.mark.parametrize(
-    "name", ["Rep", "LPObj", "LPStatus", "LPSolver"]
-)
+@pytest.mark.parametrize("name", ["Rep", "LPObj", "LPStatus", "LPSolver"])
 def test_gmp_rep(name: str) -> None:
     assert issubclass(getattr(cdd, name), IntEnum)
     assert getattr(cdd, name) is getattr(cdd.gmp, name)
