18667ef5b2d2a70c6fc6b070d2fa2cf901c47888
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-24 13:29:27 +0100

    Test more special cases.

--- test/test_issue35.py
+++ test/test_issue35.py
@@ -19,3 +19,15 @@ def test_issue35_3() -> None:
     mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
     assert cdd.matrix_canonicalize_linearity(mat) == ({0}, [None])
     assert mat.array == []
+
+
+def test_issue35_4() -> None:
+    mat = cdd.matrix_from_array([], rep_type=cdd.RepType.INEQUALITY)
+    assert cdd.matrix_redundancy_remove(mat) == (set(), [])
+    assert mat.array == []
+
+
+def test_issue35_5() -> None:
+    mat = cdd.matrix_from_array([], rep_type=cdd.RepType.INEQUALITY)
+    assert cdd.matrix_canonicalize_linearity(mat) == (set(), [])
+    assert mat.array == []
