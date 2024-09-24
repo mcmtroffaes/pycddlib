f8d49f274e92b4ae4c0434797b549a2f4c3c13f0
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-24 13:24:32 +0100

    Correct test.

--- test/test_issue35_2.py
+++ test/test_issue35_2.py
@@ -4,4 +4,4 @@ import cdd
 # this should not segfault
 def test_issue35_2() -> None:
     mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
-    cdd.matrix_canonicalize(mat)
+    cdd.matrix_redundancy_remove(mat)
