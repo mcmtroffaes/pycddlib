f2f81eeee77131fca0a702df921cf0a012fcdc53
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-24 13:32:06 +0100

    One more test.

--- test/test_issue35.py
+++ test/test_issue35.py
@@ -4,9 +4,10 @@ import pytest
 
 # this should not segfault
 @pytest.mark.skip  # temporarily disabled to check other tests
-def test_issue35() -> None:
+def test_issue35_1() -> None:
     mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
-    cdd.matrix_canonicalize(mat)
+    assert cdd.matrix_canonicalize(mat) == ({0}, None)
+    assert mat.array == []
 
 
 def test_issue35_2() -> None:
@@ -23,11 +24,17 @@ def test_issue35_3() -> None:
 
 def test_issue35_4() -> None:
     mat = cdd.matrix_from_array([], rep_type=cdd.RepType.INEQUALITY)
-    assert cdd.matrix_redundancy_remove(mat) == (set(), [])
+    assert cdd.matrix_canonicalize(mat) == (set(), set(), [])
     assert mat.array == []
 
 
 def test_issue35_5() -> None:
+    mat = cdd.matrix_from_array([], rep_type=cdd.RepType.INEQUALITY)
+    assert cdd.matrix_redundancy_remove(mat) == (set(), [])
+    assert mat.array == []
+
+
+def test_issue35_6() -> None:
     mat = cdd.matrix_from_array([], rep_type=cdd.RepType.INEQUALITY)
     assert cdd.matrix_canonicalize_linearity(mat) == (set(), [])
     assert mat.array == []
