24a0908ad92c95e0cfa17ba7ca25d89a7e137315
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-24 13:17:14 +0100

    Add segfault tests for other canonicalize functions.

---
+++ test/test_issue35_2.py
@@ -0,0 +1,7 @@
+import cdd
+
+
+# this should not segfault
+def test_issue35_2() -> None:
+    mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
+    cdd.matrix_canonicalize(mat)
