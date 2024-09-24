e1f8cd2d26321c7d8ec4efb64a07f7db89dc379b
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2024-09-24 13:26:41 +0100

    Merge tests.

--- test/test_issue35_3.py
+++
@@ -1,7 +0,0 @@
-import cdd
-
-
-# this should not segfault
-def test_issue35_3() -> None:
-    mat = cdd.matrix_from_array([[0, 0, 0]], rep_type=cdd.RepType.INEQUALITY)
-    cdd.matrix_canonicalize_linearity(mat)
